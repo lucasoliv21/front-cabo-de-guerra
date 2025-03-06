5c4af96358c6823b556f132af52307b103547424
SilvioDoMine <arthurhenrique@live.com>
2025-03-06 00:17:05 -0300

    Implementação da página de websocket myserver

---
+++ pages/myserver.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+const ws = new WebSocket('ws://localhost:9502');
+
+ws.onopen = function() {
+    console.log('Conectado!');
+};
+
+ws.onmessage = function(e) {
+    console.log('Mensagem recebida: ' + e.data);
+};
+
+ws.onclose = function() {
+    console.log('Desconectado!');
+};
+
+ws.onerror = function(err) {
+    console.error('Erro: ' + err);
+};
+
+</script>
+
+<template>
+    <div>
+        Oi!
+    </div>  
+</template>
