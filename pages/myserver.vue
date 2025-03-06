a22343bdec9506a03910c469e0b22ab2659e95c9
SilvioDoMine <arthurhenrique@live.com>
2025-03-06 00:31:16 -0300

    Recebemos e exibimos state do jogo

--- pages/myserver.vue
+++ pages/myserver.vue
@@ -1,34 +1,47 @@
 <script setup>
 
-const ws = new WebSocket('ws://localhost:9502');
-
 const connection = ref('pending');
 
-ws.onopen = function() {
-    console.log('Conectado!');
-    connection.value = 'connected';
-};
+const state = ref({});
 
-ws.onmessage = function(e) {
-    console.log('Mensagem recebida: ' + e.data);
-};
+onMounted(() => {
+    const ws = new WebSocket('ws://localhost:9502');
 
-ws.onclose = function() {
-    console.log('Desconectado!');
-    connection.value = 'disconnected';
-};
+    ws.onopen = function() {
+        console.log('Conectado!');
+        connection.value = 'connected';
+    };
 
-ws.onerror = function(err) {
-    console.error('Erro:', err);
-    connection.value = 'disconnected';
-};
+    ws.onmessage = function(e) {
+        // console.log('Mensagem recebida: ' + e.data);
+        state.value = JSON.parse(e.data);
+        console.log(state.value);
+    };
+
+    ws.onclose = function() {
+        console.log('Desconectado!');
+        connection.value = 'disconnected';
+    };
+
+    ws.onerror = function(err) {
+        console.error('Erro:', err);
+        connection.value = 'disconnected';
+    };
+})
 
 </script>
 
 <template>
     <div>
-        <div v-if="connection === 'disconnected'">Erro de conexão! Atualize a página.</div>
-        <div v-else-if="connection === 'pending'">Conectando...</div>
-        <div v-else>Conectado!</div>
+        <div v-if="connection === 'disconnected'"><p>Erro de conexão! Atualize a página.</p></div>
+        <div v-else-if="connection === 'pending'"><p>Conectando...</p></div>
+
+        <!-- Conectado -->
+        <div v-else>
+            <p>Conectado!</p>
+            <p>Time casa: {{ state.homeName }}</p>
+            <p>Time visitante: {{ state.awayName }}</p>
+            <p>{{ state.homeVotes }} x {{ state.awayVotes }}</p>
+        </div>
     </div>  
 </template>
