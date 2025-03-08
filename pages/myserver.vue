129af28e345cacedabb13fe3aff65b545509b6c3
LucasPopan <lucascodantas@hotmail.com>
2025-03-08 02:12:08 -0300

    Adaptando o frontend para receber o novo modo de dados e adicionando o winrate

--- pages/myserver.vue
+++ pages/myserver.vue
@@ -2,7 +2,10 @@
 
 const connection = ref('pending');
 
-const state = ref({});
+const state = ref({
+  game: {},
+  stats: {}, 
+});
 
 // @TODO - Resolver essa gambiarra pro jogo ter acesos a ref websocket
 const websocket = ref(null);
@@ -23,10 +26,17 @@ onMounted(() => {
         ws.send('send-state');
     };
 
-    ws.onmessage = function(e) {
-        // console.log('Mensagem recebida: ' + e.data);
-        state.value = JSON.parse(e.data);
-        console.log(state.value);
+    ws.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+
+      if (data.game) {
+        state.value.game = data.game;
+      }
+      if (data.stats) {
+        state.value.stats = data.stats;
+      }
+
+      console.log(state.value.game.status);
     };
 
     ws.onclose = function() {
@@ -41,30 +51,27 @@ onMounted(() => {
 })
 
 const gameStatus = computed(() => {
-    switch(state.value.status) {
-        case 'waiting':
-            return 'Aguardando início';
-        case 'running':
-            return 'Em andamento';
-        case 'finished':
-            return 'Finalizado';
-        default:
-            return '--';
-    };
+  const status = state.value.game?.status;
+  switch (status) {
+    case 'waiting': return 'Aguardando início';
+    case 'running': return 'Em andamento';
+    case 'finished': return 'Finalizado';
+    default: return '--';
+  }
 });
 
 const gameWinner = computed(() => {
-    if (state.value.status !== 'finished') {
-        return '--';
-    }
+  if (state.value.game?.status !== 'finished') {
+    return '--';
+  }
 
-    if (state.value.homeVotes === state.value.awayVotes) {
-        return 'Empate';
-    }
+  if (state.value.game.homeVotes === state.value.game.awayVotes) {
+    return 'Empate';
+  }
 
-    return state.value.homeVotes > state.value.awayVotes 
-        ? state.value.homeName 
-        : state.value.awayName;
+  return state.value.game.homeVotes > state.value.game.awayVotes
+    ? state.value.game.homeName
+    : state.value.game.awayName;
 });
 
 </script>
@@ -87,7 +94,7 @@ const gameWinner = computed(() => {
             <p class="absolute bg-green-500 top-5 left-5 rounded-md text-xs p-1 text-white uppercase font-bold select-none">Conectado</p>
 
             <!-- Team showoff -->
-            <div v-if="state.status === 'waiting'">
+            <div v-if="state.game.status === 'waiting'">
                 <div class="flex flex-col justify-center items-center bg-gradient-to-b from-[#8B5A2B] via-[#A97142] to-[#5C4033] min-h-screen">
                     <div class="bg-white container items-center mx-2 p-2 gap-4 rounded flex flex-col">
                         <p class="font-bold text-4xl text-center">Próximo confronto:</p>
@@ -95,8 +102,8 @@ const gameWinner = computed(() => {
                             
                             <!-- Home -->
                             <div class="w-1/3">
-                                <img :src="state.homeFlag" alt="home" class="w-20 h-20 mx-auto">
-                                <p class="text-center">{{ state.homeName }}</p>
+                                <img :src="state.game.homeFlag" alt="home" class="w-20 h-20 mx-auto">
+                                <p class="text-center">{{ state.game.homeName }}</p>
                             </div>
 
                             <!-- VS -->
@@ -105,8 +112,8 @@ const gameWinner = computed(() => {
 
                             <!-- Away -->
                             <div class="w-1/3">
-                                <img :src="state.awayFlag" alt="home" class="w-20 h-20 mx-auto">
-                                <p class="text-center">{{ state.awayName }}</p>
+                                <img :src="state.game.awayFlag" alt="home" class="w-20 h-20 mx-auto">
+                                <p class="text-center">{{ state.game.awayName }}</p>
                             </div>
 
                         </div>
@@ -124,7 +131,7 @@ const gameWinner = computed(() => {
             </div>
 
             <!-- Voting -->
-            <div v-if="state.status === 'running'">
+            <div v-if="state.game.status === 'running'">
                 <div class="flex flex-col justify-center items-center bg-gradient-to-b from-[#4CAF50] via-[#388E3C] to-[#1B5E20] min-h-screen">
                     <div class="bg-white container items-center mx-2 p-2 gap-4 rounded flex flex-col">
                         <p class="font-bold text-center text-4xl">Vote no seu time favorito:</p>
@@ -132,8 +139,8 @@ const gameWinner = computed(() => {
                             
                             <!-- Home -->
                             <div class="w-1/3">
-                                <img :src="state.homeFlag" alt="home" class="w-20 h-20 mx-auto">
-                                <p class="text-center">{{ state.homeName }}</p>
+                                <img :src="state.game.homeFlag" alt="home" class="w-20 h-20 mx-auto">
+                                <p class="text-center">{{ state.game.homeName }}</p>
                             </div>
 
                             <!-- VS -->
@@ -142,15 +149,15 @@ const gameWinner = computed(() => {
 
                             <!-- Away -->
                             <div class="w-1/3">
-                                <img :src="state.awayFlag" alt="home" class="w-20 h-20 mx-auto">
-                                <p class="text-center">{{ state.awayName }}</p>
+                                <img :src="state.game.awayFlag" alt="home" class="w-20 h-20 mx-auto">
+                                <p class="text-center">{{ state.game.awayName }}</p>
                             </div>
 
                         </div>
 
                         <div class="flex gap-5 justify-around items-center">
-                            <button @click="vote('home')" :disabled="state.status !== 'running'" class="bg-blue-500 text-white cursor-pointer p-2 rounded-md">Vote {{ state.homeName }}</button>
-                            <button @click="vote('away')" :disabled="state.status !== 'running'" class="bg-blue-500 text-white cursor-pointer p-2 rounded-md">Vote {{ state.awayName }}</button>
+                            <button @click="vote('home')" :disabled="state.game.status !== 'running'" class="bg-blue-500 text-white cursor-pointer p-2 rounded-md">Vote {{ state.homeName }}</button>
+                            <button @click="vote('away')" :disabled="state.game.status !== 'running'" class="bg-blue-500 text-white cursor-pointer p-2 rounded-md">Vote {{ state.awayName }}</button>
                         </div>
                     </div>
                     
@@ -158,18 +165,22 @@ const gameWinner = computed(() => {
             </div>
 
             <!-- Winner screen -->
-            <div v-if="state.status === 'finished'">
+            <div v-if="state.game.status === 'finished'">
                 <div class="flex flex-col justify-center items-center bg-gradient-to-r from-[#FFD700] via-[#FFC107] to-[#B8860B] min-h-screen">
                     <div class="bg-white container items-center mx-2 p-2 gap-4 rounded flex flex-col">
                         <p class="font-bold text-center text-4xl">Vencedor da Rodada</p>
                         <div class="flex w-full gap-5 justify-around items-center">
                             
                             <!-- Home -->
-                            <div :class="{'bg-black/5': gameWinner === state.homeName}" class="w-1/3 flex py-2 rounded-lg flex-col items-center">
-                                <p class="text-7xl">{{ state.homeVotes }}</p>
+                            <div :class="{'bg-black/5': gameWinner === state.game.homeName}" class="w-1/3 flex py-2 rounded-lg flex-col items-center">
+                                <p class="text-7xl">{{ state.game.homeVotes }}</p>
                                 <br>
-                                <img :src="state.homeFlag" alt="home" class="w-20 h-20 mx-auto">
-                                <p class="text-center">{{ state.homeName }}</p>
+                                <img :src="state.game.homeFlag" alt="home" class="w-20 h-20 mx-auto">
+                                <p class="text-center">{{ state.game.homeName }}</p>
+                                <p v-if="state.stats[state.game.homeName]">
+                                    Win Rate: 
+                                    {{ (state.stats[state.game.homeName].winRate * 100).toFixed(2) }}%
+                                  </p>
                             </div>
 
                             <!-- VS -->
@@ -177,11 +188,15 @@ const gameWinner = computed(() => {
 
 
                             <!-- Away -->
-                            <div :class="{'bg-black/5': gameWinner === state.awayName}" class="w-1/3 flex py-2 rounded-lg flex-col items-center">
-                                <p class="text-7xl">{{ state.awayVotes }}</p>
+                            <div :class="{'bg-black/5': gameWinner === state.game.awayName}" class="w-1/3 flex py-2 rounded-lg flex-col items-center">
+                                <p class="text-7xl">{{ state.game.awayVotes }}</p>
                                 <br>
-                                <img :src="state.awayFlag" alt="home" class="w-20 h-20 mx-auto">
-                                <p class="text-center">{{ state.awayName }}</p>
+                                <img :src="state.game.awayFlag" alt="home" class="w-20 h-20 mx-auto">
+                                <p class="text-center">{{ state.game.awayName }}</p>
+                                <p v-if="state.stats[state.game.awayName]">
+                                    Win Rate: 
+                                    {{ (state.stats[state.game.awayName].winRate * 100).toFixed(2) }}%
+                                </p>
                             </div>
 
                         </div>
