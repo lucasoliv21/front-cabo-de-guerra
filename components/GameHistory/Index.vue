1e9ea6e1daf3d0813d801e8b22f424802a4b9ff5
SilvioDoMine <arthurhenrique@live.com>
2025-03-10 21:35:23 -0300

    Fazendo componente funcionar bem no pc

--- components/GameHistory/Index.vue
+++ components/GameHistory/Index.vue
@@ -2,10 +2,38 @@
 </script>
 
 <template>
-    <div class="flex bg-white py-1 w-full rounded scrollbar-hide overflow-auto">
+    <div class="flex container bg-white py-1 w-full rounded scrollbar-hide overflow-auto">
 
         <!-- Grid scroll -->
         <div class="flex flex-shrink-0 gap-1 first:ml-1 last:mr-1">
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
+            <GameHistoryItem />
+
             <GameHistoryItem />
 
             <GameHistoryItem />
--- components/GameHistory/Item.vue
+++ components/GameHistory/Item.vue
@@ -3,7 +3,7 @@
 
 <template>
     <!-- Grid item -->
-    <div class="bg-black/25 p-1 grow flex gap-2 text-sm items-center rounded">
+    <div class="bg-black/25 first:bg-black/50 p-1 grow flex gap-2 text-sm items-center rounded">
         <img src="https://img.sofascore.com/api/v1/team/2817/image" class="p-0.5 w-6 h-6 bg-white rounded" alt="team-home">
         <p>vs</p>
         <img src="https://img.sofascore.com/api/v1/team/2817/image" class="p-0.5 w-6 h-6" alt="team-away">
