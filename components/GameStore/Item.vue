3ec2e6186b7f97afbfa2b031d9a632bb3ea2dae9
SilvioDoMine <arthurhenrique@live.com>
2025-03-25 10:53:34 -0300

    Agora custo ficará pequeno em telas grandes

--- components/GameStore/Item.vue
+++ components/GameStore/Item.vue
@@ -17,7 +17,7 @@ const props = defineProps({
                 <p class="text-lg font-semibold text-black">{{ shopItem.name }}</p>
                 <p class="font-medium text-gray-500">{{ shopItem.description }}</p>
             </div>
-            <div class="text-sm cursor-pointer py-0.5 px-4 border-1 border-[#F7971E] hover:from-[#F7971E] hover:to-[#FFD200] rounded">
+            <div class="text-sm md:w-fit cursor-pointer py-0.5 px-4 border-1 border-[#F7971E] hover:from-[#F7971E] hover:to-[#FFD200] rounded">
                 Custo: <strong>{{ shopItem.price }} pontos</strong>
             </div>
             <button class="border-purple-200 disabled:bg-purple-900/30 disabled:text-white disabled:border-transparent border-2 px-4 py-0.5 cursor-pointer rounded-xl text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700">
