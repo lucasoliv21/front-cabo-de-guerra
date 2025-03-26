93e563b919e888cf62361345e026b5e5be8867f0
SilvioDoMine <arthurhenrique@live.com>
2025-03-25 21:17:36 -0300

    Adicionado métodos para enviar mensagem no novo formato

--- stores/websocket.js
+++ stores/websocket.js
@@ -1,12 +1,25 @@
 export const useWebsocket = defineStore('websocket', () => {
-    const ws = ref({});
+    /** @type {WebSocket} */
+    const ws = ref(null);
 
     const connect = (url) => {
         ws.value = new WebSocket(url);
     }
 
+    const send = (type, payload) => {
+        const message = {
+            type: type,
+            payload: payload,
+        }
+
+        ws.value.send(
+            JSON.stringify(message)
+        );
+    }
+
     return {
         ws,
         connect,
+        send,
     };
 });
